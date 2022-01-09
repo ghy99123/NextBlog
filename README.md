dd13df3be8adf9ab677f282ba69be67de8936059
Ghy4Real <2941879036@qq.com>
2022-01-09 16:05:11 +0800

    edit README.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # 个人博客前台
+个人博客前台页面，采用 Next.js 服务端渲染 + 静态页面渲染
+
+## 技术栈
+- Next.js - 基于 React 的框架，支持**服务端渲染 SSR**和**静态页面生成 SSG**，有利于SEO
+- React (hooks)
+- Antd - 基于 React 的组件库
--- utils/urls.js
+++ utils/urls.js
@@ -1,5 +1,5 @@
 
-const urlPrefix = process.env.NODE_ENV==='development'?'http://localhost:7003/default/':'http://api.ghyyy.top/default/'
+const urlPrefix = process.env.NODE_ENV === 'development' ? 'http://localhost:7003/default/' : 'http://api.ghyyy.top/default/'
 
 export const urls = {
   articles: urlPrefix + 'articles',  //获取所有文章
