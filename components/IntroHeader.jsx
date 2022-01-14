b212924df1013d6bdde2eb5319b5f346c61f83a6
Ghy4Real <2941879036@qq.com>
2022-01-14 21:13:43 +0800

    image optimization

--- components/IntroHeader.jsx
+++ components/IntroHeader.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Image from 'next/image'
 import Style from '../styles/components/IntroHeader.module.css'
 
 export default function Introheader(props) {
   const {src, title, subtitle} = props
   return (
-    <header className={Style.introHeader} style={{backgroundImage: `url(${src})`}}>
+    <header className={Style.introHeader}>
+      <Image 
+        className={Style.bgImg}
+        layout='fill'
+        alt='Backgroud Image'
+        src={src}
+      />
       <div className={Style.container}>
         <h3 className={Style.title}>{title}</h3>
         <div className={Style.subtitle}>{subtitle}</div>
